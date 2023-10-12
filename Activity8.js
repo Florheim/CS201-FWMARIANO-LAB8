12633a422770c3fa74516db79a5905f64172922e
Florheim <fwizard0719@gmail.com>
2023-10-12 19:44:17 +0800

    feat: added my javascirpt codes into my html

---
+++ Activity8.js
@@ -0,0 +1,37 @@
+// STUDENT INFORMATION
+const studentInfo = {
+    lastName: "Mariano",
+    firstName: "Florheim Wizard",
+    middleName: "Villena",
+    section: "BSCS 2A",
+    labNo: 8,
+};
+
+console.log ("Name: " + studentInfo.firstName + " " + studentInfo.middleName + " " + studentInfo.lastName);
+console.log ("Section: " + studentInfo.section);
+console.log ("Laboratory Number: " + studentInfo.labNo);
+
+// ACT 8
+
+const favSingers = ["Rick Astley", "Taylor Swift", "Moira Dela Torre"];
+console.log ("My Favorite Singers: ");
+console.log (favSingers);
+
+const favNumbers = [16, 4, 7, 8];
+console.log ("My Favorite Numbers:");
+console.log (favNumbers);
+
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log ("ACCESSING ELEMENTS: ");
+console.log ("favSingers[0]: " + favSingers[0]);
+console.log ("favSingers[1]: " + favSingers[1]);
+console.log ("favSingers[2]: " + favSingers[2]);
+console.log ("favNumbers[0]: " + favNumbers[0]);
+console.log ("favNumbers[1]: " + favNumbers[1]);
+console.log ("favNumbers[2]: " + favNumbers[2]);
+console.log ("favNumbers[3]: " + favNumbers[3]);
+console.log ("mixedArr[0]: " + mixedArr[0]);
+console.log ("mixedArr[1]: " + mixedArr[1]);
+console.log ("mixedArr[2]: " + mixedArr[2]);
+console.log ("mixedArr[3]: " + mixedArr[3]);
